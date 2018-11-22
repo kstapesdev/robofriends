363347e66b43bd50421bc4d0e828791b53952d69
kstapesdev <kstapesdev@gmail.com>
2018-11-22 10:54:25 +0700

    adding robofriends

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return(
+        <div style={{overflowY: 'scroll', height: '100%', overflow: 'hidden'}}>
+            {props.children}
+        </div>
+    );
+};
+
+export default Scroll;
